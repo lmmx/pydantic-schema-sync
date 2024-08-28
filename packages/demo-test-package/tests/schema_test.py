8487fdaa8c811aec6258afd7313b450f7f2c4ac2
Louis Maddox <louismmx@gmail.com>
2024-08-28 22:18:51 +0100

    refactor: single enum

--- packages/demo-test-package/tests/schema_test.py
+++ packages/demo-test-package/tests/schema_test.py
@@ -5,10 +5,5 @@ from pytest import mark
 @mark.pydantic_schema_sync
 class SyncedSchemas(Enum):
     input = "demo_test_package.data_model.In"
+    message = "demo_test_package.data_model.Message"
     output = "demo_test_package.data_model.Out"
-
-
-@mark.pydantic_schema_sync
-class SyncedSchemasExtra(Enum):
-    extra = "demo_test_package.data_model.Extra"
-    etc = "demo_test_package.data_model.Etc"
