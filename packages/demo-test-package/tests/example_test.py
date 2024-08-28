13666aa409a2cdc61863b3de5d3b59b17cf8a170
Louis Maddox <louismmx@gmail.com>
2024-08-28 18:29:56 +0100

    test: simple test

--- packages/demo-test-package/tests/example_test.py
+++ packages/demo-test-package/tests/example_test.py
@@ -1,4 +1,6 @@
 from pytest import importorskip
 
-def test_sync():
+def test_simple(capsys):
     importorskip("examples.simple")
+    captured = capsys.readouterr()
+    assert captured.out == "Hello, World!\nCount: 1\nCount: 3\nCount: 5\n"
